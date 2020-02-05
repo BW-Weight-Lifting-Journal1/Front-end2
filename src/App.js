29431a962e03b041aa4dec3bd6f11cac67ca0e76
Chase-42 <chaseofthecollins@gmail.com>
2020-02-04 18:21:39 -0700

    Delete Functionality for WorkoutCard...switched to localStorage for userId to fix re-loading issue...also, cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,36 +1,25 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext } from "react";
 import { Switch, Route } from "react-router-dom";
-import axios from "axios";
 import "./App.css";
+// Components
 import Welcome from "./components/User/WelcomePage";
 import EditWorkout from "./components/EditWorkout"
 import GuestLogin from "./components/User/GuestLogin";
 import GuestRegister from "./components/User/GuestRegister";
+import Dashboard from "./components/Dashboard";
+// Utils 
 import ProtectedRoute from "./Auth/ProtectedRoute";
 import axiosWithAuth from "./Auth/axiosWithAuth";
 // Contexts
 import { WorkOutContext } from "./contexts/WorkOutContext";
 import { ExcerciseContext } from "./contexts/ExcerciseContext";
-import Dashboard from "./components/Dashboard";
 
 function App(props) {
   const [workOut, setWorkOut] = useState([]);
   const [excercise, setExcercise] = useState({});
-  const [userId, setUserId] = useState();
-
-  useEffect(() => {
-    axiosWithAuth()
-      .get("/api/workouts")
-      .then(res => {
-        // console.log(res.data);
-        setWorkOut(res.data);
-        // setWorkOut(res.data);
-      });
-  }, [setWorkOut]);
 
   const addWorkout = item => {
     console.log("item passed to addWorkout in App.js", item);
-    // setWorkOut(...workOut, item);
     console.log("workOut in App.js", workOut);
     axiosWithAuth()
       .post("api/workouts", item)
@@ -40,26 +29,30 @@ function App(props) {
       })
       .catch(err => console.log(err));
   };
-  const addUserId = item => {
-    setUserId(item);
-    console.log("userId", userId);
-  };
+
   const addExcercise = item => {
-    // axios
-    //   .post(
-    //     "https://workout-journal-backend.herokuapp.com/api/workout/excercise",
-    //     item
-    //   )
-    //   .then(response => {
-    //     setExcercise(response.data);`
-    //   })
-    //   .catch(err => console.log(err));
+    axiosWithAuth()
+      .post("api/workout/excercise", item)
+      .then(response => {
+        console.log(response)
+        setExcercise(response.data);
+      })
+      .catch(err => console.log(err));
   };
+
+  const deleteItem = id => {
+    console.log("delete id", id)
+    axiosWithAuth()
+      .delete(`/api/workouts/${id}`)
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+  }
+
   const excrcse1 = useContext(ExcerciseContext);
   const wrkout1 = useContext(WorkOutContext);
 
   return (
-    <WorkOutContext.Provider value={{ workOut, addWorkout, userId, setUserId, setWorkOut }}>
+    <WorkOutContext.Provider value={{ workOut, addWorkout, setWorkOut, deleteItem }}>
       <ExcerciseContext.Provider value={{ excercise, addExcercise }}>
         <Switch>
           <Route exact path="/" component={Welcome} />
